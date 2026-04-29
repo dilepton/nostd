5e082d44da3ab3eae0dfb7fa5ef6c2fdc9923bb3
dilepton <dilepton@outlook.com>
2026-04-29 17:41:27 +0300

    add io_uring: fixed syscalls

--- src/io/io_uring/syscall.cpp
+++ src/io/io_uring/syscall.cpp
@@ -2,7 +2,6 @@
 
 #include "syscall.hpp"
 #include "sysno.hpp"
-#include "error.hpp"
 
 using namespace nostd;
 
@@ -12,10 +11,10 @@ res_t io_uring::io_uring_setup(u32 entries, io_uring_params* p) noexcept {
   return __nostd_syscall2(sysno::IO_URING_SETUP, entries, p);
 }
 
-error_t io_uring::io_uring_register() noexcept {
-  return E::NOSYS;
+error_t io_uring::io_uring_register(i32 fd, u32 opcode, const void* arg, u32 nr_args) noexcept {
+  return __nostd_syscall4(sysno::IO_URING_REGISTER, fd, opcode, arg, nr_args);
 }
 
 res_t io_uring::io_uring_enter(u32 to_submit, u32 min_complete, u32 flags, void* arg, usize argsz) noexcept {
-  return __nostd_syscall6(sysno::IO_URING_ENTER, _ringfd, to_submit, min_complete, flags, arg, argsz);
+  return __nostd_syscall6(sysno::IO_URING_ENTER, _enterfd, to_submit, min_complete, flags, arg, argsz);
 }
